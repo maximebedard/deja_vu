4603bd54bcc86fd0765e56625b20472fbd3be5dd
Maxime Bedard <maxime.bedard@shopify.com>
2017-11-22 19:32:40 -0500

    Add utility functions to make booting the app easier

--- lib/deja_vu.rb
+++ lib/deja_vu.rb
@@ -6,3 +6,13 @@ require "deja_vu/proxy"
 require "deja_vu/request_serializer"
 require "deja_vu/app"
 require "deja_vu/version"
+
+module DejaVu
+  def self.root
+    File.dirname(__dir__)
+  end
+
+  def self.boot
+    File.join(root, "bin", "config.ru")
+  end
+end
